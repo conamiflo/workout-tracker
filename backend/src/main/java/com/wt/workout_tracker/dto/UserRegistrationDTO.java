779ce46323a8986dd2ded283d4cdaabfcc1276cb
Nemanja Stjepanović <nemanjastjepanovic222@gmail.com>
2025-08-12 14:53:26 +0200

    feat: implemented user service and repository

---
+++ backend/src/main/java/com/wt/workout_tracker/dto/UserRegistrationDTO.java
@@ -0,0 +1,26 @@
+package com.wt.workout_tracker.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserRegistrationDTO {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+}
